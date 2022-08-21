13bff77770d7805a0c5aa8888026da81194ef4eb
Patryk <patrykzdebik@gmail.com>
2022-08-21 21:10:33 +0200

    Create function playRound with conditionals
    Created function playRound which takes two arguments:
    playerSelection and computerSelection. Added if else
    statements, which evaluates who is the winner of the game.
    Invoked function with prompt and getComputerChoice function.

--- script.js
+++ script.js
@@ -11,9 +11,33 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    //IF user wins, display string: "You win! <Smth> beats <smth>
+    //ELSE, display string: "You lose! <Smth> beats <smth>"
+    if (playerSelection === computerSelection) {
+        console.log(`Draw! ${playerSelection} and ${computerSelection}.`)
+    }else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+    }else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+    }else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+    }else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+    }else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+    }else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+    }else {
+        console.log("Invalid weapon");
+    }
+}
 
 //Ask user for an input: Rock, Paper or Scissors
-//IF user wins, display string: "You win! <Smth> beats <smth>
-//ELSE, display string: "You lose! <Smth> beats <smth>"
-//Pass random data, and user input to function playRound, which evaluates which data wins
+//Pass getComputerChoice(), and user input to function playRound, which evaluates who wins
+playRound(prompt("Choose your weapon:", ""), getComputerChoice());
+
+
+
 //Loop for 5 rounds and display each win
+

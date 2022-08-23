cee2f52da72aceab48f937780163f09b338e5dc2
Patryk <patrykzdebik@gmail.com>
2022-08-23 11:07:26 +0200

    Create game(), and modify playRound()
    Added (for game function):
    - two variables which store players and computers
    points.
    - for loop which evaluates points
    - console.log displaying final number of points
    - conditionals evaluating who wins at last
    
    Added (for playRound function):
    - return values which are evaluated in conditional nested
    in for loop in game()

--- script.js
+++ script.js
@@ -17,26 +17,32 @@ function playRound(playerSelection, computerSelection) {
     //ELSE, display string: "You lose! <Smth> beats <smth>"
     if (playerSelection === computerSelection) {
         console.log(`Draw! ${playerSelection} and ${computerSelection}.`)
-    }else if (playerSelection === "rock" && computerSelection === "Paper") {
+        return "Draw";
+    }else if (playerSelection === "Rock" && computerSelection === "Paper") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+        return "Lose";
     }else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+        return "Win";
     }else if (playerSelection === "Paper" && computerSelection === "Rock") {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+        return "Win";
     }else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+        return "Lose";
     }else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`)
+        return "Lose";
     }else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+        return "Win";
     }else {
         console.log("Invalid weapon");
     }
 }
 
-//Ask user for an input: Rock, Paper or Scissors
+//Ask user for an input: Rock, Paper or Scissors and pass it to function
 //Make playerSelection parameter case insensitive
-
 function getPlayerChoice() {
     let askPlayer = prompt("Choose your weapon:", "");
     let toLower = askPlayer.toLowerCase();
@@ -47,7 +53,32 @@ function getPlayerChoice() {
 }
 
 //Pass getComputerChoice(), and user input to function playRound, which evaluates who wins
-playRound(getPlayerChoice(), getComputerChoice());
+//playRound(getPlayerChoice(), getComputerChoice());
 
-//Loop for 5 rounds and display current status
+//Play a 5 round game that keeps score and reports a winner or loser at the end.
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getPlayerChoice(), getComputerChoice());
+
+        if (playRound === "Lose"){
+            computerPoints += 1;
+        }else if (playRound === "Win") {
+            playerPoints += 1;
+        } 
+    }
+
+    console.log("You: " + playerPoints);
+    console.log("Computer: " + computerPoints);
+
+    if (playerPoints > computerPoints) {
+        console.log("You Win!");
+    }else if (computerPoints > playerPoints) {
+        console.log("You Lose!");
+    }else {
+        console.log("Draw!");
+    }
+}
 
